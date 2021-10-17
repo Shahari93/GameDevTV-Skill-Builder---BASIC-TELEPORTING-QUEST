ed2a32dd56e53d6323117d7c7ebc0abf026b6adf
Shahari93 <shaharilani93@gmail.com>
2021-10-17 18:54:17 +0300

    Bounce pad
    - Added new bounce pad to the level.
    - Whenever the player step on the bounce pad he get launched into the air

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -1,21 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 // GameDev.tv Challenge Club. Got questions or want to share your nifty solution?
 // Head over to - http://community.gamedev.tv
+
+using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
     [SerializeField] float jumpForce = 1000f;
-    
+    private Vector3 upVector = Vector3.up;
+
     void OnTriggerEnter(Collider other)
     {
         // Challenge 1: JumpyJumpy(other);
+        JumpForce(other);
     }
 
-    void JumpyJumpy(Collider other)
+    void JumpForce(Collider other)
     {
-
+        GameObject player = other.gameObject;
+        if (player.CompareTag("Player"))
+        {
+            Rigidbody rb = player.GetComponent<Rigidbody>();
+            rb.AddForce(upVector * jumpForce, ForceMode.Force);
+        }
     }
 }
